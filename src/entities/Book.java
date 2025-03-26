dd9c19e26c79158b3aca5265a53d5fa68fded762
Rafael Tavares <tavares.rfl@gmail.com>
2025-03-26 19:24:53 -0300

    feat: add book entity

---
+++ src/entities/Book.java
@@ -0,0 +1,87 @@
+package entities;
+
+import java.util.Objects;
+
+public class Book {
+
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+    private Double year;
+    private String publisher;
+
+    public Book() {
+    }
+
+    public Book(Long id, String title, String author, String isbn, Double year, String publisher) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.year = year;
+        this.publisher = publisher;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Double getYear() {
+        return year;
+    }
+
+    public void setYear(Double year) {
+        this.year = year;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
