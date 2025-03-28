d0fef8732da162f5dd52ab73da343d1db9d8c4d6
Rafael Tavares <tavares.rfl@gmail.com>
2025-03-28 10:38:38 -0300

    test: add books

--- src/application/Application.java
+++ src/application/Application.java
@@ -12,6 +12,20 @@ public class Application {
                 "978-8520918432", 1954, "Editora Nova Fronteira"));
         BookService.insert(new Book(3L, "Torto Arado", "Itamar Vieira Junior",
                 "978-6580309313", 2019, "Editora Todavia"));
+        BookService.insert(new Book(4L, "O Acontecimento", "Annie Ernaux",
+                "978-6589733553", 2000, "Fósforo Editora"));
+        BookService.insert(new Book(5L, "O Apanhador no Campo de Centeio", "J.D. Salinger",
+                "978-6580309030", 1951, "Editora Todavia"));
+        BookService.insert(new Book(6L, "A Festa do Bode", "Mario Vargas Llosa",
+                "978-8579620638", 2000, "Editora Alfaguara"));
+        BookService.insert(new Book(7L, "Crime e Castigo", "Fiódor Dostoiévski",
+                "978-8525416476", 1866, "Editora L&PM"));
+        BookService.insert(new Book(8L, "A Morte de Ivan Ilitch", "Lev Tolstói",
+                "978-8573263596", 1886, "Editora 34"));
+        BookService.insert(new Book(9L, "Tudo é Rio", "Carla Madeira",
+                "978-6555871784", 2014, "Editora Record"));
+        BookService.insert(new Book(10L, "O Senhor do Lado Esquerdo", "Alberto Mussa",
+                "978-8501101143", 2011, "Editora Record"));
 
         for (Book book : BookService.findAll()) {
             System.out.println(book);
--- src/repositories/BookRepository.java
+++ src/repositories/BookRepository.java
@@ -3,13 +3,12 @@ package repositories;
 import entities.Book;
 
 import java.util.LinkedList;
-import java.util.List;
 
 public abstract class BookRepository {
 
-    private static List<Book> list = new LinkedList<>();
+    private static LinkedList<Book> list = new LinkedList<>();
 
-    public static List<Book> getList() {
+    public static LinkedList<Book> getList() {
         return list;
     }
 }
