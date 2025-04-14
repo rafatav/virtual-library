51f4a031cc05b27bf445d360ae923baad85b2d02
Rafael Tavares <tavares.rfl@gmail.com>
2025-04-13 23:19:12 -0300

    feat: add temporary interface
    This is just a prototype

--- src/application/Application.java
+++ src/application/Application.java
@@ -1,19 +1,31 @@
 package application;
 
-import entities.Book;
 import entities.User;
 import resources.Seed;
 import services.BookService;
 
+import java.util.Scanner;
+
 public class Application {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Seed.books();
 
-        User user = new User(1L, "Wellington Monteiro", "wellington@gmail.com", "123456");
+        System.out.print("Digite o seu nome: ");
+        String name = sc.nextLine();
+        User user = new User(1L, name, name.toLowerCase().replaceAll("\\s", "") +
+                "@gmail.com", "123456");
 
-        for (Book book : BookService.findAll()) {
-            System.out.println(book);
+        user.getBrowsingHistory().add(BookService.findById(1L));
+
+        System.out.println("\n||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+        System.out.println("||||||||||||||||||| BIBLIOTECA VIRTUAL DALTON TREVISAN |||||||||||||||||||||");
+        System.out.println("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+
+        System.out.println("\nOlá, " + user.getName() + ". Bem-vindo à Biblioteca Virtual Dalton Trevisan");
+
+
         }
     }
-}
+
